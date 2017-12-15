2b203de4243e573bb022dd53cadbba64e7729457
Keepo <Keepitk.xprezz@yahoo.com>
2017-12-14 21:54:12 -0700

    1st app code, working well so far/ blue background

--- 1st app/ViewController.swift
+++ 1st app/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Thelabel: UILabel!
+    
+    var tapCount = 0
+    
+    
+    @IBAction func buttonTapped(sender: AnyObject) {Thelabel.text = "Hello There!"
+    }
+    @IBAction func ndbuttonTapped(sender: AnyObject) {Thelabel.text = "Buttons are cool!"
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            Thelabel.text = "You've outtapped yourself"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.blueColor()
+        
     }
 
     override func didReceiveMemoryWarning() {
